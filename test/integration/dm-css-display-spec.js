a0f8cad1f90bf4e42318b30ca6234dd6c33c2a46
Rajinder Yadav <devguy.ca@gmail.com>
2017-12-18 00:09:37 -0500

    Test name correction.

--- test/integration/dm-css-display-spec.js
+++ test/integration/dm-css-display-spec.js
@@ -4,55 +4,55 @@ describe("Display", () => {
     cy.visit("/test/web-pages/display.html");
   });
 
-  it("None", () => {
+  it("show", () => {
     cy.get("#show")
       .should("have.class", "show")
       .should("have.css", "display", "inline")
       .should("be.visible");
   });
-  it("None", () => {
+  it("hide", () => {
     cy.get("#hide")
       .should("have.class", "hide")
       .should("have.css", "display", "none")
       .should("be.not.visible");
   });
-  it("None", () => {
+  it("none", () => {
     cy.get("#d-none")
       .should("have.class", "d-none")
       .should("have.css", "display", "none")
       .should("be.not.visible");
   });
-  it("None", () => {
+  it("inline", () => {
     cy.get("#d-inline")
       .should("have.class", "d-inline")
       .should("have.css", "display", "inline");
   });
-  it("None", () => {
+  it("inline-block", () => {
     cy.get("#d-inline-block")
       .should("have.class", "d-inline-block")
       .should("have.css", "display", "inline-block");
   });
-  it("None", () => {
+  it("block", () => {
     cy.get("#d-block")
       .should("have.class", "d-block")
       .should("have.css", "display", "block");
   });
-  it("None", () => {
+  it("flex", () => {
     cy.get("#d-flex")
       .should("have.class", "d-flex")
       .should("have.css", "display", "flex");
   });
-  it("None", () => {
+  it("inline-flex", () => {
     cy.get("#d-inline-flex")
       .should("have.class", "d-inline-flex")
       .should("have.css", "display", "inline-flex");
   });
-  it("None", () => {
+  it("grid", () => {
     cy.get("#d-grid")
       .should("have.class", "d-grid")
       .should("have.css", "display", "grid");
   });
-  it("None", () => {
+  it("inline-grid", () => {
     cy.get("#d-inline-grid")
       .should("have.class", "d-inline-grid")
       .should("have.css", "display", "inline-grid");
