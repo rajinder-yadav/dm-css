b1b935533907f748fa08271cc5acdf18f27f63ce
Rajinder Yadav <devguy.ca@gmail.com>
2018-01-01 14:32:54 -0500

    Removed alias.

--- test/integration/float-spec.js
+++ test/integration/float-spec.js
@@ -5,29 +5,14 @@ describe("Float", () => {
   });
 
   it("Float alias left", () => {
-    cy.get("#float-l")
-      .should("have.class", "float-l")
-      .should("have.css", "float", "left");
-  });
-  it("Float left", () => {
     cy.get("#fll")
       .should("have.class", "fll")
       .should("have.css", "float", "left");
-  });
-  it("Float alias right", () => {
-    cy.get("#float-r")
-      .should("have.class", "float-r")
-      .should("have.css", "float", "right");
   });
   it("Float right", () => {
     cy.get("#flr")
       .should("have.class", "flr")
       .should("have.css", "float", "right");
-  });
-  it("Float alias none", () => {
-    cy.get("#float-n")
-      .should("have.class", "float-n")
-      .should("have.css", "float", "none");
   });
   it("Float none", () => {
     cy.get("#fln")
