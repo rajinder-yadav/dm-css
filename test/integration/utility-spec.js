a9ab9af560f7f380358f75776fb0d37e9cbdcdab
Rajinder Yadav <devguy.ca@gmail.com>
2018-01-02 15:47:38 -0500

    New opacity tests.

--- test/integration/utility-spec.js
+++ test/integration/utility-spec.js
@@ -30,4 +30,47 @@ describe("Size", () => {
         .should("have.css", "align-items", "center");
     });
   });
+
+  context("Opacity", () => {
+    it("Level 0", () => {
+      cy.get(".opacity0")
+        .should("have.class", "opacity0",)
+        .should("have.css", "opacity", "0");
+    });
+    it("Level 1", () => {
+      cy.get(".opacity1")
+        .should("have.class", "opacity1",)
+        .should("have.css", "opacity", "0.05");
+    });
+    it("Level 2", () => {
+      cy.get(".opacity2")
+        .should("have.class", "opacity2",)
+        .should("have.css", "opacity", "0.1");
+    });
+    it("Level 3", () => {
+      cy.get(".opacity3")
+        .should("have.class", "opacity3",)
+        .should("have.css", "opacity", "0.28");
+    });
+    it("Level 4", () => {
+      cy.get(".opacity4")
+        .should("have.class", "opacity4",)
+        .should("have.css", "opacity", "0.46");
+    });
+    it("Level 5", () => {
+      cy.get(".opacity5")
+        .should("have.class", "opacity5",)
+        .should("have.css", "opacity", "0.64");
+    });
+    it("Level 6", () => {
+      cy.get(".opacity6")
+        .should("have.class", "opacity6",)
+        .should("have.css", "opacity", "0.82");
+    });
+    it("Level 7", () => {
+      cy.get(".opacity7")
+        .should("have.class", "opacity7",)
+        .should("have.css", "opacity", "1");
+    });
+  });
 });
