37e6ad477af126e7ee8a6a822e13b2055f4b2536
Dev Guy <devguy.ca@gmail.com>
2023-11-30 18:44:38 -0500

    feat: new opacity levels

--- test/integration/utility.cy.js
+++ test/integration/utility.cy.js
@@ -39,36 +39,51 @@ describe("Size", () => {
     it("Level 1", () => {
       cy.get(".opacity1")
         .should("have.class", "opacity1",)
-        .should("have.css", "opacity", "0.05");
+        .should("have.css", "opacity", "0.1");
     });
     it("Level 2", () => {
       cy.get(".opacity2")
         .should("have.class", "opacity2",)
-        .should("have.css", "opacity", "0.1");
+        .should("have.css", "opacity", "0.2");
     });
     it("Level 3", () => {
       cy.get(".opacity3")
         .should("have.class", "opacity3",)
-        .should("have.css", "opacity", "0.28");
+        .should("have.css", "opacity", "0.3");
     });
     it("Level 4", () => {
       cy.get(".opacity4")
         .should("have.class", "opacity4",)
-        .should("have.css", "opacity", "0.46");
+        .should("have.css", "opacity", "0.4");
     });
     it("Level 5", () => {
       cy.get(".opacity5")
         .should("have.class", "opacity5",)
-        .should("have.css", "opacity", "0.64");
+        .should("have.css", "opacity", "0.5");
     });
     it("Level 6", () => {
       cy.get(".opacity6")
         .should("have.class", "opacity6",)
-        .should("have.css", "opacity", "0.82");
+        .should("have.css", "opacity", "0.6");
     });
     it("Level 7", () => {
       cy.get(".opacity7")
         .should("have.class", "opacity7",)
+        .should("have.css", "opacity", "0.7");
+    });
+    it("Level 8", () => {
+      cy.get(".opacity8")
+        .should("have.class", "opacity8",)
+        .should("have.css", "opacity", "0.8");
+    });
+    it("Level 9", () => {
+      cy.get(".opacity9")
+        .should("have.class", "opacity9",)
+        .should("have.css", "opacity", "0.9");
+    });
+    it("Level 10", () => {
+      cy.get(".opacity10")
+        .should("have.class", "opacity10",)
         .should("have.css", "opacity", "1");
     });
   });
