04332b0c167db0eae26ad91b1eb5864452b12970
Dev Guy <devguy.ca@gmail.com>
2023-12-09 03:58:43 -0500

    refactor: updated

--- npm-up.sh
+++ npm-up.sh
@@ -1,8 +1,13 @@
 # !/usr/bin/env node
 
 rm -rf node_modules
-mv package-lock.json package-lock.tmp.json
-mv package.json package.tmp.json
-cp package.dev.json package.json
+mv package-lock.json package-lock.json.bak
+mv package.json package.json.bak
+cp package.json.t package.json
 
 npm i -D autoprefixer cssnano sass postcss postcss-cli browser-sync cypress gazeall npm-run-all shx
+
+if [ "$1" = "-" ]; then
+  rm -rf node_modules
+  rm -f package-lock.json yarn.lock
+fi
